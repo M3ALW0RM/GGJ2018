7db2405eb8ba23315e1c355bbab480bab2094465
grandbaton <grandbaton@gmail.com>
2018-01-27 23:31:12 -0500

    Almost working queue

--- GGJEddyTeam/Assets/C#/Cannister.cs
+++ GGJEddyTeam/Assets/C#/Cannister.cs
@@ -7,11 +7,20 @@ public class Cannister : MonoBehaviour {
 
     private string message;
     private Text caption;
-    private bool pressed;
+    private Animator anim;
+    private float animOffset = 0.05f; //value in animation "time" in between two cans
 
-    private void ButtonPressed()
+    public float speed = 0.01f, sinSpeed = 100f;
+
+    [HideInInspector]
+    public bool outOfRange = false;
+    public float slidePos;
+    public int number;
+
+    public void ButtonPressed()
     {
-        
+        outOfRange = true;
+        anim.SetBool("Eject", true);
     }
 
     public void SetCaption( string txt )
@@ -19,20 +28,37 @@ public class Cannister : MonoBehaviour {
         caption.text = txt;
     }
 
-    public void Initialize( string s )
+    public void Initialize( string s, int n )
     {
         message = s;
+        number = n;
+    }
+
+    public void GoDown()
+    {
+        slidePos += animOffset;
     }
 
 	// Use this for initialization
 	void Start ()
     {
         caption = GetComponentInChildren<Text>();
+        anim = GetComponent<Animator>();
         SetCaption(message);
+        slidePos = 0.6f - animOffset * number;
+        anim.SetFloat("slidePos", slidePos);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        slidePos += Time.deltaTime * speed * Mathf.Abs(Mathf.Sin( Time.time * Time.deltaTime * sinSpeed ));
+        anim.SetFloat("slidePos", slidePos);
+
+        //Check if too far down in the anim
+        if (slidePos > 0.7f)
+        {
+            outOfRange = true;
+        }
 	}
 }
--- GGJEddyTeam/Assets/C#/CannisterQueue.cs
+++ GGJEddyTeam/Assets/C#/CannisterQueue.cs
@@ -7,26 +7,46 @@ public class CannisterQueue : MonoBehaviour {
     [SerializeField]
     private Cannister cannisterPrefab;
 
-    private Queue cannisterQueue;
+    private List<Cannister> cannisterList;
 
-    private float animOffset = 0.05f;
 
     public void AddCannister(string message)
     {
         Cannister newCan = Instantiate(cannisterPrefab, this.transform, true);
-        newCan.Initialize(message);
-        cannisterQueue.Enqueue(newCan);
+        newCan.Initialize(message, cannisterList.Count);
+        cannisterList.Add(newCan);
     }
 
 	// Use this for initialization
 	void Start ()
     {
-        AddCannister("test");
+        cannisterList = new List<Cannister>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        //DEV - REMOVE WHEN CODE IS IN TO ADD MESSAGES
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            AddCannister("test");
+        }
+
+        int canNum = cannisterList.Count;
+
+        for( int i = 0; i<canNum; i++)
+        {
+            Cannister can = cannisterList[i];
+            if( can.outOfRange )
+            {
+                for( int j = canNum-1; j>i; j--)
+                {
+                    cannisterList[j].GoDown();
+                }
+                ////////INSERT CODE FOR FIRING MESSAGE////////
+                cannisterList.Remove(can);
+                break;
+            }
+        }
 	}
 }
