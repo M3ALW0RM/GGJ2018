aac3dd6d4a7f79e05be7f1c3fddc30360ba4ed4e
EddyLejaSix <e.leja-six@isartdigital.com>
2018-01-28 14:29:13 -0500

    Script évitant de viser à gauche de l'écran

---
+++ GGJEddyTeam/Assets/C#/CanonShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonShoot : MonoBehaviour {
+
+    public CannonBehavior test;
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.mousePosition.x>416)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                test.MoveCannon();
+            }
+        }
+
+
+
+    }
+}
